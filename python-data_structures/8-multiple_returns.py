86dc5e498e02bb559de3bd80e6e1501d139ce512
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-01 18:03:04 +0200

    task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-# 9-max_integer.py
+# 8-multiple_returns.py
 
 
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
-    big = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
-    return (big)
+def multiple_returns(sentence):
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
