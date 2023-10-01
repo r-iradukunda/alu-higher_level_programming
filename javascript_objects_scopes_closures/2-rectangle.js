4d7bd8f54ec1b758fafa85238bc70e208bbdc18c
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-01 19:57:26 +0200

    JavaScript

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-
-class Rectangle{
-    constructor (w, h){
-        if (w <= 0 || h <= 0){
-            return;
-        }
-        this.width = w;
-        this.height = h; 
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-}
-
-module.exports = Rectangle
+  }
+};
