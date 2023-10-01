50f4179edc530e43b3cb27924c8796a36f1d6725
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-01 18:03:07 +0200

    python inherintance

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:    
+    """Reprsent base geometry."""
+
+    def area(self):
+        """Not yet implemented."""        
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a parameter as an integer.
+        :param name: name of the parameter
+        :param value: name of parameter to validator
+        :returns TypeError if value is not an interger
+        :returns ValueError if value is <= 0>
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
