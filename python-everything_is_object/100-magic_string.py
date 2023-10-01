4ddce9fdea4a8621de3f85179d7f509f8239398d
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-01 18:03:07 +0200

    Best school

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.iter = getattr(magic_string, 'iter', 0) + 1
-    return "BestSchool" + (", BestSchool" * (magic_string.iter - 1))
+
+def magic_string(counter=[0]):
+    counter[0] += 1
+    return ", ".join(["BestSchool"] * counter[0])
+
+for i in range(10):
+    print(magic_string())
