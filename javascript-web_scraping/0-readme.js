a4321cb03ed89d910eaa1050a77b5dd2a71d2316
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-04 03:02:13 +0200

    javascript

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 3) {
-	console.error('usage: node 0-readme.js <file-path>');
-	process.exit(1);
-}
+const path = require('path');
 
 const filePath = process.argv[2];
 
-// Read the content of the file in utf-8
 fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    // Handle errors, and print the error object
+ if (err) {
     console.error(err);
-  } else {
-    // Print the file content
+ } else {
     console.log(data);
-  }
+ }
 });
+
