da661d9eb545ad3fa2dbd8b2666f61efebdd04e2
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-04 02:14:47 +0200

    web scraping

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (process.argv.length !== 3) {
+	console.error('usage: node 0-readme.js <file-path>');
+	process.exit(i);
+}
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err);
+	     } else {
+		console.log(data);
+	     }
+});
