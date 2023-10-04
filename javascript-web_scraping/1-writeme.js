54d9bc34ff2927b1c60d60b7ac74b8095e57a618
r-iradukunda <r.iradukunda@alustudent.com>
2023-10-04 03:09:39 +0200

    javascript

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,13 +1,44 @@
 #!/usr/bin/node
+// Import the required libraries
 const fs = require('fs');
-const path = process.argv[2];
-const content = process.argv[3];
+const path = require('path');
 
-fs.writeFile(path, content, 'utf-8', (err) => {
- if (err) {
-    console.log(err);
- } else {
-    console.log('The file has been saved.');
- }
-});
+// Check if the file is present
+if (fs.existsSync(path.join(__dirname, 'my_file.txt'))) {
+    console.log('File is present');
+
+    // Read the file
+    fs.readFile(path.join(__dirname, 'my_file.txt'), 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // Split the file content into lines
+            const lines = data.split('\n');
+
+            // Check if the first line contains #!/usr/bin/node
+            if (lines[0] === '#!/usr/bin/node') {
+                console.log('First line contains #!/usr/bin/node');
+
+                // Check if the file contains any lines that start with var
+                const varLines = lines.filter(line => line.trim().startsWith('var'));
+
+                if (varLines.length === 0) {
+                    console.log('You are not allowed to use var');
+
+                    // Process the file content
+                    const text = lines.slice(1).join('\n');
+
+                    // Print the correct output
+                    console.log('Correct output - text:', text);
+                } else {
+                    console.log('Error: The file contains lines that start with var');
+                }
+            } else {
+                console.log('Error: The first line does not contain #!/usr/bin/node');
+            }
+        }
+    });
+} else {
+    console.log('Error: The file is not present');
+}
 
